a80bfaa03491ef6b0d9ea225c9429971adb5d689
legrin <danielflinx@gmail.com>
2019-08-01 23:13:52 +0300

    Updates

---
+++ precache-manifest.ac0ab3cb86bdc2303d48b784045f6c7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a7fa0a61df3af5c337fb904fffdac9f",
+    "url": "/asanpicker/index.html"
+  },
+  {
+    "revision": "f070b1b38c5a470e71f7",
+    "url": "/asanpicker/static/css/main.9e905bff.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/asanpicker/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "f070b1b38c5a470e71f7",
+    "url": "/asanpicker/static/js/main.3021174f.chunk.js"
+  },
+  {
+    "revision": "541df64d6ad8140f8a3c",
+    "url": "/asanpicker/static/js/runtime~main.9b4a7aa9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/asanpicker/static/media/logo.5d5d9eef.svg"
+  }
+]);
